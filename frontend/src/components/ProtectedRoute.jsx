3150f050ceefb355efb672bb0fb1dee0378ec396
Mohammed Shahnawaz <mdshahnawaz090605@gmail.com>
2025-08-10 21:57:19 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import  Navigate  from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, role }) => {
   const user = JSON.parse(localStorage.getItem("user"));
