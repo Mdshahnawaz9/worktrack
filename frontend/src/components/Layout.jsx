73c39107b9cead60a77b22f9fbad88448395141d
Mohammed Shahnawaz <mdshahnawaz090605@gmail.com>
2025-08-14 22:44:00 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,25 +1,20 @@
-// src/components/Layout.jsx
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import { useDarkMode } from "./DarkModeProvider";
 
-const Layout = ({ onLogout, children }) => {
-  const { darkMode, toggleDarkMode } = useDarkMode();
+/**
+ * Page wrapper: Navbar + Sidebar + content area
+ */
+export default function Layout({ children }) {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 ml-64">
-        <Navbar
-          onLogout={onLogout}
-          darkMode={darkMode}
-          toggleDarkMode={toggleDarkMode}
-        />
-        <main className="p-6">{children}</main>
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
+      <Navbar onToggleSidebar={() => setOpen((v) => !v)} />
+      <div className="flex">
+        <Sidebar open={open} onClose={() => setOpen(false)} />
+        <main className="flex-1 p-4 sm:p-6 sm:ml-64">{children}</main>
       </div>
     </div>
   );
-};
-
-export default Layout;
+}
